a6639430d17f2058f3690da2bbb13ea39a0e0122
olgashaposhnik <50341504+olgashaposhnik@users.noreply.github.com>
2020-05-28 00:15:42 +0300

    доработка замечаний

--- gulpfile.js
+++ gulpfile.js
@@ -83,7 +83,7 @@ gulp.task("html", function () {
 
 gulp.task("copy", function () {
   return gulp.src([
-    "source/fonts/**/*.{woff,woff2}",
+    "source/fonts/**/*.{woff,woff2,ttf}",
     "source/img/**",
     "source/js/**",
     "source//*.ico"
